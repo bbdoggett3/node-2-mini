57ccd89e53e137bae777641466cd7da225541e9f
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-19 16:00:18 -0600

    add CORS policy to application

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const bookCtrl = require('./controllers/books_controller')
+const cors = require('cors');
+const bookCtrl = require('./controllers/books_controller');
 const SERVER_PORT = 4000;
 
 const app = express();
 
-app.use(express.json())
-app.use(express.static(__dirname + '/../build'))
+app.use(express.json());
+app.use(cors());
+app.use(express.static(__dirname + '/../build'));
 
 //endpoints
 app.post(`/api/books`, bookCtrl.create);
@@ -13,4 +15,4 @@ app.get(`/api/books`, bookCtrl.read);
 app.put(`/api/books/:id`, bookCtrl.update);
 app.delete('/api/books/:id', bookCtrl.delete);
 
-app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`))
+app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`));
